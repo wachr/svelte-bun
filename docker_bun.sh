f48051c1f2817db95c13272c1eb112618d49a6b7
Ray Wach <wach.ray+git@gmail.com>
2023-12-06 21:34:00 -0800

    Add pid to container name

--- docker_bun.sh
+++ docker_bun.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 
-docker run -it --rm --name $(basename `pwd`)-container  \
+docker run -it --rm  \
+    --name $(basename `pwd`)-container-$(ps -o pid= | head -n 1 | tr -d ' ')  \
     -u `id -u`:`id -g` -v `pwd`:/home/bun/app oven/bun $*
